14bf7f5c0a9ae7551eb597cefe54e63477bfce51
Nikolas McNeal <mcneal.121@osu.edu>
2023-12-23 00:06:07 -0500

    add validation

--- ml.py
+++ ml.py
@@ -29,15 +29,21 @@ target_function_time, target_function_values = rnn.sample_from_gp(t_gp, tau_targ
 
 
 readout_samples = []
+mse_values = []
 for epoch in range(epochs):    
     V = rnn.forward_pass(epoch, input_time, input_amplitude)
     readout_samples.append(np.dot(rnn.w, rnn.V))
     
-    # every 20 epochs, update weights:
-    if (epoch % 5== 0) and (epoch != 0):
+    # update the weights
+    if (epoch % 400 == 0) and (epoch != 0):  # validation epochs
+        error = np.mean(((target_function_values[3][epoch-100:epoch-1]) - (readout_samples[epoch-100:epoch-1])) ** 2)
+        # error = np.mean((target_function_values[3][epoch] - readout_samples[epoch]) ** 2)
+        print('MSE at epoch {}:'.format(epoch), error)
+        mse_values.append(error)
+
+    elif (epoch % 5 == 0) and (epoch != 0):  # training epochs
         error = sum((target_function_values[3][epoch-5:epoch-1] - readout_samples[epoch-5:epoch-1]))
         rnn.w = rnn.w + (lr * error * np.tanh(rnn.V))
-        # print(error)
 
 
 
@@ -50,3 +56,8 @@ plt.plot(target_function_values[3], label='Target function')
 plt.plot(readout_samples, label='Estimated function')
 plt.legend()
 plt.savefig('outs.png')
+
+
+plt.figure()
+plt.plot(mse_values)    
+plt.savefig('mse.png')
